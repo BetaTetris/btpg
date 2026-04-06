d85b5c9c32729d59db4efd3b9310d3a55a93a8eb
adrien1018 <adrien1018@users.noreply.github.com>
2026-04-05 17:58:56 -0700

    Emscripten version

--- wasm/build.sh
+++ wasm/build.sh
@@ -5,16 +5,16 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
 git submodule update --init
 
 cd ../emsdk
-EMSDK_VER=5.0.5
+EMSDK_VER=4.0.21
 ./emsdk activate $EMSDK_VER || ( ./emsdk install $EMSDK_VER && ./emsdk activate $EMSDK_VER )
 source ./emsdk_env.sh
 
 cd ../wasm
-emcc.py -O2 -std=c++20 -fexceptions -sALLOW_MEMORY_GROWTH -sWASM_BIGINT -sENVIRONMENT=web -sEXPORT_ES6 \
+em++ -O2 -std=c++20 -fexceptions -sALLOW_MEMORY_GROWTH -sWASM_BIGINT -sENVIRONMENT=web -sEXPORT_ES6 \
     -o tetris.js --emit-tsd tetris.d.ts --emit-symbol-map \
     tetris.cpp binding/*.cpp tetris/frame_sequence.cpp -lembind
 
-# emcc -O2 -std=c++20 -fexceptions -sALLOW_MEMORY_GROWTH -sWASM_BIGINT -sENVIRONMENT=web -sSINGLE_FILE \
+# em++ -O2 -std=c++20 -fexceptions -sALLOW_MEMORY_GROWTH -sWASM_BIGINT -sENVIRONMENT=web -sSINGLE_FILE \
 #     -o tetris-single.js \
 #     tetris.cpp binding/*.cpp tetris/frame_sequence.cpp -lembind
 
